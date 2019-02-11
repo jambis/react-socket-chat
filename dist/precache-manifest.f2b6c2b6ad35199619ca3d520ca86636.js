49fc4cfb2f59fea6f926ecbd9777ee23357ec5b8
jambis <imjaamees@gmail.com>
2019-02-11 14:51:53 -0600

    Included dist folder

---
+++ dist/precache-manifest.f2b6c2b6ad35199619ca3d520ca86636.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1086b7c059a1c4d0eb51",
+    "url": "/static/js/main.1086b7c0.chunk.js"
+  },
+  {
+    "revision": "b80334c43fbbf901fedc",
+    "url": "/static/js/1.b80334c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8661f7c8f036302863f634c9c934a77a",
+    "url": "/index.html"
+  }
+];
